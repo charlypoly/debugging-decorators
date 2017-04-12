766af4b36f3d1122fe2439513a6d9aa46351af5f
Charly POLY <charly.poly@dashlane.com>
2017-04-12 17:06:03 +0200

    refactor + specs

--- lib/index.ts
+++ lib/index.ts
@@ -1,28 +1,63 @@
 import { v4 as uuidV4 } from 'uuid';
-// Method decorator
-//   log `${className}.${methodName}` and arguments when method called
-export function log(target: any, methodName: String, descriptor: PropertyDescriptor) {
-  let original = descriptor.value;
-  descriptor.value = function () {
-    let className = target.constructor.toString().match(/function\s+([a-zA-Z]+)\(/)[1];
-    console.log(`${className}.${methodName}`, arguments);
-    return original.apply(this, Array.from(arguments));
-  };
-  return descriptor;
+
+
+export interface logOptions {
+  pattern?: string;
+  logger?: Function;
 }
 // Method decorator
+//   log `${className}.${methodName}` and arguments when method called
+export function log({ pattern = `{className}.{methodName}: {arguments}`, logger = console.log }: logOptions) {
+
+  function applyPattern(className: string, methodName: string, args: IArguments) {
+    return  pattern.replace('{className}', className).
+              replace('{methodName}', methodName).
+              replace('{arguments}', Array.from(args).join(', '));
+  }
+
+  return (target: any, methodName: string, descriptor: PropertyDescriptor) => {
+    let original = descriptor.value;
+    descriptor.value = function () {
+      let className = target.constructor.toString().match(/function\s+([a-zA-Z]+)\(/)[1];
+      logger(applyPattern(className, methodName, arguments));
+      return original.apply(this, Array.from(arguments));
+    };
+    return descriptor;
+  }
+}
+
+export interface ITimerConsole {
+  time(label: string): void;
+  timeEnd(label: string): void;
+}
+
+export interface timeOptions {
+  labelPattern?: string;
+  timer?: ITimerConsole;
+}
+
+// Method decorator
 //   log time execution when method called
-export function time(target: any, methodName: String, descriptor: PropertyDescriptor) {
-  let original = descriptor.value;
-  descriptor.value = function () {
-    let className = target.constructor.toString().match(/function\s+([a-zA-Z]+)\(/)[1];
-    let uuid = uuidV4();
-    let label = `${className}.${methodName} - ${uuid}`;
-    let result: any;
-    console.time(label);
-    result = original.apply(this, Array.from(arguments));
-    console.timeEnd(label);
-    return result;
-  };
-  return descriptor;
+export function time({ labelPattern = `{className}.{methodName} - {uuid}`, timer = console }: timeOptions) {
+
+  function applyLabelPattern(className: string, methodName: string, uuid: string) {
+    return  labelPattern.replace('{className}', className).
+              replace('{methodName}', methodName).
+              replace('{uuid}', uuid);
+  }
+
+  return (target: any, methodName: string, descriptor: PropertyDescriptor) => {
+    let original = descriptor.value;
+    descriptor.value = function () {
+      let className = target.constructor.toString().match(/function\s+([a-zA-Z]+)\(/)[1];
+      let uuid = uuidV4();
+      let label = applyLabelPattern(className, methodName, uuid);
+      let result: any;
+      timer.time(label);
+      result = original.apply(this, Array.from(arguments));
+      timer.timeEnd(label);
+      return result;
+    };
+    return descriptor;
+  }
 }
